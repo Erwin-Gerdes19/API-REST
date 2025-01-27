a905197683d4ef717551d0e5e5755ca64c52cd1f
Erwin-Gerdes19 <erwin.gerdes19@gmail.com>
2025-01-27 08:03:58 -0300

    mongo

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const routes = require('./routes');
+const moongose = require('mongoose');
+// Conectar a MongoDB   
+moongose.Promise = global.Promise;
+moongose.connect('mongodb://localhost/restapis', {
+    useNewUrlParser: true
+});
 // crear el servidor
 const app = express();
-
+// cargar las rutas
+app.use('/', routes());
 // puerto
 app.listen(5000);
